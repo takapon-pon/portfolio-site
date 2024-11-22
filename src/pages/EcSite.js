44184fbaf7e103170a0c8fe0df25a08771977eab
takapon-pon <takapon063@gmail.com>
2024-11-22 15:22:01 +0900

    Initial commit

---
+++ src/pages/EcSite.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EcSite = () => {
+    return (
+        <div>
+            <h1>アパレルECサイト見本</h1>
+            <p>ここにアパレルECサイトのデザインを作成します。</p>
+        </div>
+    );
+};
+
+export default EcSite;
